e018efd87f3f604c501318904b157ea09266a221
Alan Christensen <alan@christensen.org.nz>
2026-04-22 21:35:08 +1200

    Convert app.json to app.config.ts with app variants and splash screen (#81)
    * swap app.json to app.config.ts
    
    * app variants for side by side
    
    * .gitignore
    
    * update deps
    
    * Lockfile
    
    * rough splash and icon

---
+++ app/app.config.ts
@@ -0,0 +1,62 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const bundleId = () => {
+  if (IS_DEV) return "com.auctionready.nag.app.dev";
+  if (IS_PREVIEW) return "com.auctionready.nag.app.preview";
+  return "com.auctionready.nag.app";
+};
+
+const appName = () => {
+  if (IS_DEV) return "nag (Dev)";
+  if (IS_PREVIEW) return "nag (Preview)";
+  return "nag";
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: appName(),
+  slug: "nag",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  scheme: "nag",
+  ios: {
+    ...config.ios,
+    supportsTablet: true,
+    bundleIdentifier: bundleId(),
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
+    appleTeamId: "QNWX2YN6C5",
+  },
+  platforms: ["ios"],
+  plugins: [
+    "@react-native-community/datetimepicker",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/donkey-splash.jpeg",
+        resizeMode: "contain",
+        backgroundColor: "#8b6545",
+      },
+    ],
+    [
+      "expo-notifications",
+      {
+        mode: "development",
+      },
+    ],
+    "expo-router",
+    "expo-sqlite",
+  ],
+  extra: {
+    eas: {
+      projectId: "bc1ee86a-bc83-4f06-aa4f-d4f757d18e55",
+    },
+  },
+  owner: "nag-stable",
+});
