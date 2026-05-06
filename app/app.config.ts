630588d121ae874f2c5ff3437e21358112586f83
Alan Christensen <alan@christensen.org.nz>
2026-05-06 21:54:06 +1200

    update expo deps

--- app/app.config.ts
+++ app/app.config.ts
@@ -50,6 +50,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     "expo-router",
     "expo-sqlite",
     "expo-font",
+    "expo-web-browser",
     [
       "expo-splash-screen",
       {
