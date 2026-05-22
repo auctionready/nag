d668a73c578770efbb7d56d5d26dab4227a7aa92
Alan Christensen <alan@christensen.org.nz>
2026-05-22 18:02:39 +1200

    blog: infra fixes

--- blog/infra/index.ts
+++ blog/infra/index.ts
@@ -1,9 +1,11 @@
 import * as aws from "@pulumi/aws";
+import * as command from "@pulumi/command";
 import * as github from "@pulumi/github";
+import * as pulumi from "@pulumi/pulumi";
 
 const apex = "auctionready.co.nz";
 const subdomain = `nag.${apex}`;
-const repoOwner = "auctionready";
+const owner = "auctionready";
 
 // --- Route 53 CNAME ---
 const zone = aws.route53.getZoneOutput({ name: apex });
@@ -13,17 +15,34 @@ new aws.route53.Record("pages-cname", {
   name: subdomain,
   type: "CNAME",
   ttl: 300,
-  records: [`${repoOwner}.github.io`],
+  records: [`${owner}.github.io`],
 });
 
-const repository = new github.Repository("site", {
+const repo = new github.Repository("site", {
   name: "nag",
   visibility: "public",
 });
 
 // --- GitHub Pages config ---
 new github.RepositoryPages("pages", {
-  repository: repository.name,
+  repository: repo.name,
   buildType: "workflow",
   cname: subdomain,
 });
+
+// --- github-pages environment, no branch restrictions ---
+new github.RepositoryEnvironment("pages-env", {
+  repository: repo.name,
+  environment: "github-pages",
+  // omit deploymentBranchPolicy entirely = all branches allowed
+});
+
+new command.local.Command(
+  "enforce-https",
+  {
+    create: pulumi.interpolate`gh api -X PUT repos/${owner}/${repo.name}/pages -F https_enforced=true`,
+    // Re-run if the repo identity changes
+    triggers: [repo.id],
+  },
+  { dependsOn: [repo] },
+);
